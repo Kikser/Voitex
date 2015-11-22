8bdcb19f64027221bea2a7fdbcb64470e10c1fd1
Kikser <kr.arsovski@gmail.com>
2015-11-22 02:57:18 +0100

    aaaa

--- www/js/services.js
+++ www/js/services.js
@@ -8,26 +8,31 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Ben Sparrow',
     Phone: '+38970278896',
-    face: 'img/ben.png'
+    face: 'img/ben.png',
+    lastCall:'two days ago'
   }, {
     id: 1,
     name: 'Max Lynx',
     Phone: '+3816008896',
-    face: 'img/max.png'
+    face: 'img/max.png',
+    lastCall:'5 min ago'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     Phone: '+380340033221',
+    lastCall:'30 min ago',
     face: 'img/adam.jpg'
   }, {
     id: 3,
     name: 'Perry Governor',
     Phone: '+389756886669',
+    lastCall:'an hour ago',
     face: 'img/perry.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
     Phone: '+3781100348',
+    lastCall:'yesterday ago',
     face: 'img/mike.png'
   }];
 
