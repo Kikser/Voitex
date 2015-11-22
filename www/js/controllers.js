e3cc518d0195ac90146b55927f4c436eba27e83d
joconolimits <joconolimits@gmail.com>
2015-11-22 11:40:10 +0100

    Hidden tabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,36 +5,46 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('ChatsCtrl', function ($scope, Chats, $rootScope) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    $rootScope.$on('$stateChangeStart',
+    function (event, toState, toParams, fromState, fromParams) {
+        if (fromState.name == 'tab.chat-detail' && toState.name == 'tab.chats')
+        {
+            $rootScope.tabsHidden = false;
+        }
+        else if (fromState.name == 'tab.activeCall' && toState.name == 'tab.chats') {
+            $rootScope.tabsHidden = false;
+        }
+    })
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats, $rootScope) {
+    $scope.chat = Chats.get($stateParams.chatId);
+    $rootScope.tabsHidden = true;
 
 })
 
-    //.controller('jocoCtrl', function ($scope) { })
-.controller('FaverCtrl', function ($scope) {})
+.controller('FaverCtrl', function ($scope) { })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+  
 })
-.controller('activeCallCtrl', function ($scope) {
+.controller('activeCallCtrl', function ($scope, $rootScope) {
+    $rootScope.tabsHidden = true;
     $scope.callText = '';
     var commands = {
         '*val': function (val) {
@@ -57,11 +67,10 @@ angular.module('starter.controllers', [])
     }
 
     // Text to Speech
-    $scope.TTS = {value: ''};
+    $scope.TTS = { value: '' };
     console.log($scope.TTS.value);
     $scope.talk = function () {
         responsiveVoice.speak($scope.TTS.value, "UK English Female", { rate: 0.7 });
     }
 });
-
 
