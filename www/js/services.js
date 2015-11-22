31c5abc31c099a0bbecd4f5955b96ac8320dac0b
joconolimits <joconolimits@gmail.com>
2015-11-22 10:26:38 +0100

    Details view

--- www/js/services.js
+++ www/js/services.js
@@ -9,31 +9,36 @@ angular.module('starter.services', [])
     name: 'Ben Sparrow',
     Phone: '+38970278896',
     face: 'img/ben.png',
-    lastCall:'two days ago'
+    lastCall: 'two days ago',
+      mail: 'ben@gmail.com'
   }, {
     id: 1,
     name: 'Max Lynx',
     Phone: '+3816008896',
     face: 'img/max.png',
-    lastCall:'5 min ago'
+    lastCall: '5 min ago',
+    mail: 'm.lynx@gmail.com'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     Phone: '+380340033221',
     lastCall:'30 min ago',
-    face: 'img/adam.jpg'
+    face: 'img/adam.jpg',
+    mail: 'bradleyson@gmail.com'
   }, {
     id: 3,
     name: 'Perry Governor',
     Phone: '+389756886669',
     lastCall:'an hour ago',
-    face: 'img/perry.png'
+    face: 'img/perry.png',
+    mail: 'perry@gmail.com'
   }, {
     id: 4,
     name: 'Mike Harrington',
     Phone: '+3781100348',
     lastCall:'yesterday ago',
-    face: 'img/mike.png'
+    face: 'img/mike.png',
+    mail: 'mike@gmail.com'
   }];
 
   return {
